1fd37a10c89e00ce2397bed398671befc65c851a
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-08-09 14:00:25 +0000

    Feat: db model png image

--- src/models.py
+++ src/models.py
@@ -7,42 +7,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    display_name = Column(String(250), nullable=False)
+    name = Column(Integer, nullable=False)
+    display_name = Column(String(50), nullable=False)
     email = Column(String(250), nullable=False)
-    password = Column(String(100))
-    avatar = Column(String(250))
+    password = Column(String(100), nullable=False)
+    avatar = Column(String(250), nullable=True)
 
 
-class Favorites(Base):
-    __tablename__ = 'favorites'
-    user_id = Column(Integer, ForeignKey('user.id'))
-    name = Column(String(250))
-    url = Column(String(250))
-
-class Posts(Base):
-    __tablename__ = 'posts'
+class User_Like_Planets(Base):
+    __tablename__ = "user_like_planets"
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    planet = relationship(User)
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User_Like_Character(Base):
+    __tablename__ = "user_like_character"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey("character.id"))
+    character = relationship(User)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    rotation_period = Column(String(50), nullable=True)
+    diameter = Column(String(50), nullable=True)
+    terrain = Column(String(50), nullable=True)
+    population = Column(String(50), nullable=True)
+    image = Column(String(200), nullable=True)
+
+
+class Character(Base):
+    __tablename__ = "character"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=True)
+    hair_color = Column(String(50), nullable=True)
+    eye_color = Column(String(20), nullable=True)
+    birth_year = Column(String(20), nullable=True)
+    gender =Column(String(50), nullable=True)
+    homeworld = Column(String(50), ForeignKey("planet.id"))
+    homeworld = relationship(Planet)
+
+
 
     def to_dict(self):
         return {}
